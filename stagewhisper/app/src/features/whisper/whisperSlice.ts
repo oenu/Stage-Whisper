d57651a6b26d5c83b6b78f101ded49ffb7af2947
oenu <51684443+oenu@users.noreply.github.com>
2022-10-04 15:27:51 -0400

    init whisper slice

--- stagewhisper/app/src/features/whisper/whisperSlice.ts
+++ stagewhisper/app/src/features/whisper/whisperSlice.ts
@@ -1,20 +1,55 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { entry } from '../../../electron/types/types';
+import { RootState } from '../../redux/store';
+
+// WhisperSlice
 // Slice for managing requests to whisper and the queue of requests
 
-const initialState = {
-  requests: [],
-  queue: []
+export type queueEntry = {
+  // Represents an entry in the queue
+  id: string; // Id of the transcription
+  entry: entry; // Entry that requested the transcription
+  status: 'idle' | 'loading' | 'succeeded' | 'failed'; // Status of the transcription
+};
+
+export type whisperState = {
+  // Represents the state of the whisper slice
+  queue: queueEntry[]; // Queue of transcriptions
+  activeEntry: string | null; // Id of the active entry
+  status: 'idle' | 'loading' | 'succeeded' | 'failed' | 'disabled'; // Status of the slice
+};
+
+const initialState: whisperState = {
+  queue: [],
+  activeEntry: null,
+  status: 'idle'
 };
 
 const whisperSlice = createSlice({
   name: 'whisper',
   initialState,
   reducers: {
-    addRequest: (state, action) => {
-      state.requests.push(action.payload);
+    addRequestToQueue: (state, action) => {
+      state.queue.push(action.payload);
     }
   }
 });
 
-export const { addRequest } = whisperSlice.actions;
+// Active Transcription Thunk
+// This thunk controls the passing of entries to whisper and the queueing of entries
+const activeTranscriptionThunk = createAsyncThunk(
+  'whisper/activeTranscriptionThunk',
+  async (entry: entry, { dispatch, getState }) => {
+    // Get the current state
+    const state = getState() as RootState;
+    // Get the current queue
+    const queue = state.whisper.queue;
+    // Get the current active entry
+    const activeEntry = state.whisper.activeEntry;
+    // Get the current status
+  }
+);
+
+// export const {} = whisperSlice.actions;
 
 export default whisperSlice.reducer;
