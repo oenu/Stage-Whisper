f9d4e941cf6ec9af539e9390904454fd8f82a673
oenu <51684443+oenu@users.noreply.github.com>
2022-10-05 19:10:21 -0400

    moved hooks inside component

--- stagewhisper/app/src/features/entries/components/AudioPlayer.tsx
+++ stagewhisper/app/src/features/entries/components/AudioPlayer.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
-
-const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-
-// Calls main process to get audio file, returned in format Uint8Array that can be recomposed into an audio file
-const handleAudioUint8Array = async (filePath: string) => {
-  window.Main.fetchAudioFile(filePath).then((audio) => {
-    const audioBlob = new Blob([audio], { type: 'audio/mp3' });
-    setAudioBlob(audioBlob);
-  });
-};
+import React, { useEffect, useState } from 'react';
 
 function AudioPlayer({ filePath }: { filePath: string }) {
-  if (filePath) {
-    handleAudioUint8Array(filePath);
-  }
+  const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+
+  // Calls main process to get audio file, returned in format Uint8Array that can be recomposed into an audio file
+  const handleAudioUint8Array = async (filePath: string) => {
+    console.time('AudioPlayer: Fetch Audio');
+    window.Main.fetchAudioFile(filePath).then((audio) => {
+      const audioBlob = new Blob([audio], { type: 'audio/mp3' });
+      setAudioBlob(audioBlob);
+      console.timeEnd('AudioPlayer: Fetch Audio');
+    });
+  };
+
+  useEffect(() => {
+    if (filePath) {
+      handleAudioUint8Array(filePath);
+    }
+  }, []);
 
   return (
     <div>
